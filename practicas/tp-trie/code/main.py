54017b45ecef28e4d424b8405a5c5175fab56f6e
franciscoAragona <aragonafran9@gmail.com>
2024-04-19 18:30:27 -0300

    insert recursivo y lee una secuencia de un nodo 0

--- practicas/tp-trie/code/main.py
+++ practicas/tp-trie/code/main.py
@@ -1,5 +1,7 @@
+import trie
+
 class Trie:
-	root = None
+    root = None
 
 class TrieNode:
     parent = None
@@ -7,17 +9,32 @@ class TrieNode:
     key = None
     isEndOfWord = False
 
+def lee(node):
+    if node.key != None:
+        print(node.key)
+        
+    else:
+        return
+    if node.children != None:
+        lee(node.children[0])
+    
 T=Trie()
 T.root=TrieNode()
 A=TrieNode()
 A.key="d"
-T.root.children=[A]
-lee(T.root)
+B=TrieNode()
+B.key="s"
+C=TrieNode()
+C.key="e"
+A.children=[C]
+T.root.children=[A,B]
+lee(A)
+trie.insertR(C,"do")
+print("!")
+lee(A)
 
-def lee(node):
-    if node != None:
-        print(node.key)
-        lee(node.children)
-    else:
-        return
+
+
+
+
     
